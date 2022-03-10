085fdcf122d5678fb443606cc4f5d3b877a89890
Hugo Patrício <hugopatricio51@gmail.com>
2022-03-10 19:48:14 -0300

    Adicionando arquivo de preenchimento da tabela de pedidos

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Item 1',
+        productNumber: 3,
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Item 2',
+        productNumber: 3,
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Item 3',
+        productNumber: 3,
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Item 4',
+        productNumber: 3,
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Item 5',
+        productNumber: 3,
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+
+]
