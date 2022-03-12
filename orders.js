f1806f3247e46ae5d3b40874e90360b2b120bab4
Hugo Patrício <hugopatricio51@gmail.com>
2022-03-12 11:33:47 -0300

    Alterando dados

--- orders.js
+++ orders.js
@@ -1,31 +1,31 @@
 const Orders = [
     {
-        productName: 'Item 1',
-        productNumber: 3,
+        productName: 'GPS Drone',
+        productNumber: 745,
+        paymentStatus: 'Due',
+        shipping: 'Success'
+    },
+    {
+        productName: ' Professional Racket M4000 ',
+        productNumber: 3421,
+        paymentStatus: 'Due',
+        shipping: 'Success'
+    },
+    {
+        productName: 'White Pegasus Polo T-Shirt',
+        productNumber: 982,
         paymentStatus: 'Due',
         shipping: 'Pending'
     },
     {
-        productName: 'Item 2',
-        productNumber: 3,
+        productName: 'Energetic Energy 354ml 24x',
+        productNumber: 5248,
         paymentStatus: 'Due',
         shipping: 'Pending'
     },
     {
-        productName: 'Item 3',
-        productNumber: 3,
-        paymentStatus: 'Due',
-        shipping: 'Pending'
-    },
-    {
-        productName: 'Item 4',
-        productNumber: 3,
-        paymentStatus: 'Due',
-        shipping: 'Pending'
-    },
-    {
-        productName: 'Item 5',
-        productNumber: 3,
+        productName: 'Sports-Good Black Hat',
+        productNumber: 243,
         paymentStatus: 'Due',
         shipping: 'Pending'
     }
